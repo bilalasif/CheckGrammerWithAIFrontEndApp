db8ced2984cdff103baa9b6398b12366bc3ab90b
Bilal Asif <bilalasif435@hotmail.com>
2025-03-17 20:42:07 +0500

    setup  basic sign in sign up UI components without styling

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <label htmlFor="email">Email: </label>
+      <input
+        type="text"
+        name="email"
+        id="email"
+        placeholder="Enter your Email to Sign in"
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Enter your Password to Sign in"
+      />
+      <button>submit</button>
+
+      <Link to={"/sign-up"}>
+        <button>sign up</button>
+      </Link>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <label htmlFor="email">Email: </label>
+      <input
+        type="text"
+        name="email"
+        id="email"
+        placeholder="Enter Email to Sign Up"
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Enter Password to Sign Up"
+      />
+      <button>submit</button>
+
+      <Link to={"/sign-in"}>
+        <button>sign in</button>
+      </Link>
+    </div>
+  );
 }
 
 export default SignUp;
